383f7afeb75d0ff7785a4613b78cc7b58adeb89a
bastmaan <bastmaan@live.se>
2025-05-25 20:48:15 +0200

    bugfix 2.0

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from '../src/components/Router/Router';
-import SkeletonLoaders from './components/SkeletonLoaders/SkeletonLoaders';
+import SkeletonLoaders from './components/SkeletonLoaders/SkeletonLoaders.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
